2287e9a727b913f697c0e6c9561b4f5f5c2a108f
Jean Pierre <jptr2189@gmail.com>
2023-12-14 20:03:07 -0200

    style: Melhorando escrita dos comentários

--- Exercicios/ex002.py
+++ Exercicios/ex002.py
@@ -1,4 +1,5 @@
-# Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas
+# Faça um programa que leia o nome de uma pessoa e
+# mostre uma mensagem de boas-vindas
 
 nome = str(input("Digite seu nome: "))
 
---
+++ Exercicios/ex004.py
@@ -0,0 +1,3 @@
+# Faça um programa que leia algo pelo teclado e mostre na tela
+# o seu tipo primitivo e todas as informações possíveis sobre ele
+
