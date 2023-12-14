5b11a4f5b427b70efce0e80c46a4c5c24940d43e
Jean Pierre <jptr2189@gmail.com>
2023-12-14 18:33:26 -0200

    fix: Corrigindo bug do ex002

--- Exercicios/ex002.py
+++ Exercicios/ex002.py
@@ -1,5 +1,5 @@
 # Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas
 
-nome = input(str("Digite seu nome: "))
+nome = str(input("Digite seu nome: "))
 
 print(f"É um prazer te conhecer {nome}!")
