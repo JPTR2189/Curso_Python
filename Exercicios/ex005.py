fee4231c41aa857c6d9e0fcaf9f232580ec980ed
Jean Pierre <jptr2189@gmail.com>
2023-12-14 20:19:21 -0200

    Exercício ex005 concluído

---
+++ Exercicios/ex005.py
@@ -0,0 +1,6 @@
+# Faça um programa que leia um número Inteiro e
+# mostre na tela o seu sucessor e seu antecessor
+
+numero = int(input("Digite um número: "))
+
+print(f"Analisando o valor {numero}, seu antecessor é {numero - 1} e o sucessor é {numero + 1}")
