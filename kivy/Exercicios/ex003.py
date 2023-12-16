c74e54ab1475dc49967e0376f51e861137d07f77
Jean Pierre <jptr2189@gmail.com>
2023-12-16 17:57:29 -0200

    raw: Atualizando arquivos

---
+++ kivy/Exercicios/ex003.py
@@ -0,0 +1,6 @@
+# Crie um programa que leia dois números e mostre a soma entre eles
+
+numero1 = int(input("Digite um número: "))
+numero2 = int(input("Digite outro número: "))
+
+print(f"A soma entre {numero1} e {numero2} é igual a {numero1 + numero2}")
---
+++ kivy/Exercicios/ex007.py
@@ -0,0 +1,9 @@
+# Desenvolva um programa que leia as duas notas
+# de um aluno, calcule e mostre a sua média
+
+nota1 = int(input("Digite a primeira nota: "))
+nota2 = int(input("Digite a segunda nota: "))
+media = (nota1 + nota2) / 2
+
+print("-" * 50)
+print(f"A média do aluno é: {media}")
