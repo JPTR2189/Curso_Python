5ec510ff2feae78f69cedead40d85690c73e7560
Jean Pierre <jptr2189@gmail.com>
2023-12-16 18:23:54 -0200

    raw: Atualizando branches

--- Exercicios/ex010.py
+++
@@ -1,7 +0,0 @@
-# Crie um programa que leia quanto dinheiro uma pessoa tem na
-# carteira e mostre quantos dólares ela pode comprar
-
-dinheiro = float(input("Digite quanto dinheiro você tem na carteira: R$"))
-
-print("-" * 60)
-print(f"Com R${dinheiro} você pode comprar U$ {dinheiro / 4.914:.2f}")
