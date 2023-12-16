819895f39d2887463899cc73080259ab6c97dbfe
Jean Pierre <jptr2189@gmail.com>
2023-12-16 18:03:40 -0200

    raw: Atualizando arquivos

--- kivy/Exercicios/ex003.py
+++
@@ -1,6 +0,0 @@
-# Crie um programa que leia dois números e mostre a soma entre eles
-
-numero1 = int(input("Digite um número: "))
-numero2 = int(input("Digite outro número: "))
-
-print(f"A soma entre {numero1} e {numero2} é igual a {numero1 + numero2}")
