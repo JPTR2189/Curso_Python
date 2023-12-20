9b11f478c351555d61f8f09711a18174c231dbe3
Jean Pierre <jptr2189@gmail.com>
2023-12-20 20:33:55 -0200

    aula07 concluída

---
+++ excript/app-comerciais-kivy/aulas/aula07.py
@@ -0,0 +1,28 @@
+# Operadores
+
+print('-' * 30)
+print(100 == 100)
+print('a' == 'b')
+print('a' != 'b')
+print(False == False)
+
+print('-' * 30)
+a = (100 != 100)
+b = ('a' == 'a')
+
+print('-' * 30)
+print(a)
+print(b)
+
+print('-' * 30)
+print(3 > 3)
+print(5 < 5.1)
+print(5 <= 5.1)
+
+# Baseado na tabela ASCII
+
+print('-' * 30)
+print('a' < 'b')
+print('a' > 'b')
+print('a' <= 'A')
+print('-' * 30)
