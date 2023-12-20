87c8b33fb1d7ce0636a9b9599caab4a645be0a09
Jean Pierre <jptr2189@gmail.com>
2023-12-20 17:22:07 -0200

    style: melhorando o estilo e a visualização do código da aula04

--- excript/app-comerciais-kivy/aulas/aula04.py
+++ excript/app-comerciais-kivy/aulas/aula04.py
@@ -1,29 +1,44 @@
+print("*" * 40)
 print("SOMA")
 print("*" * 40)
+
 print(10 + 10)
 print(10 + (50 + 50))
-print("-" * 40)
 
+print()
+
+print("*" * 40)
 print("SUBTRAÇÃO")
 print("*" * 40)
+
 print(10 - 10)
 print(100 - 80)
-print("-" * 40)
 
+print()
+
+
+print("*" * 40)
 print("DIVISÃO")
 print("*" * 40)
+
 print(10 / 5)
 print(10 / 6)
-print("-" * 40)
 
+print()
+
+print("*" * 40)
 print("MULTIPLICAÇÃO")
 print("*" * 40)
+
 print(10 * 800)
 print(55 * 5)
-print("-" * 40)
 
+
+print()
+
+print("*" * 40)
 print("DIVISÃO INTEIRA")
 print("*" * 40)
+
 print(10 // 32)
 print(10 // 6)
-print("-" * 40)
