4e130469dc3312d7b2877c1758ac54d06b5e8e2f
Jean Pierre <jptr2189@gmail.com>
2023-12-20 17:55:39 -0200

    aula06 concluída

---
+++ excript/app-comerciais-kivy/aulas/aula06.py
@@ -0,0 +1,18 @@
+# Potenciação e Radiciação
+
+print("*" * 30)
+print("POTENCIAÇÃO")
+print("*" * 30)
+
+print(5 ** 8)
+print(2 ** 8)
+print(10 ** 10)
+
+print()
+
+print("*" * 30)
+print("RADICIAÇÃO")
+print("*" * 30)
+
+print(9 ** (1 / 2))  # Raiz Quadrada
+print(9 ** (1 / 3))  # Raiz Cúbica
