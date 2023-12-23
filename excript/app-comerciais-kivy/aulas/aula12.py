b840ce496a3cd5ca6b038b794dd1d8b2c750b190
Jean Pierre <jptr2189@gmail.com>
2023-12-23 14:40:12 -0200

    aula12 concluída

---
+++ excript/app-comerciais-kivy/aulas/aula12.py
@@ -0,0 +1,23 @@
+print("-" * 8)
+print(2 < 4 and 2 == 4)
+print("-" * 8)
+
+print("-" * 8)
+print(2 > 4 or 2 < 4)
+print("-" * 8)
+
+print("-" * 8)
+print(not(2 > 4 or 2 < 4))
+print("-" * 8)
+
+print("-" * 40)
+print(f"O objeto '2' é igual a '2'? {2 is 2}")
+print("-" * 40)
+
+print("-" * 8)
+print(type(2) is int)
+print("-" * 8)
+
+print("-" * 8)
+print(type(2) is float)
+print("-" * 8)
