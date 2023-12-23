ba12a672870f03051ff47f5e80bcafd85da38be6
Jean Pierre <jptr2189@gmail.com>
2023-12-23 13:37:14 -0200

    aula10 concluída

---
+++ excript/app-comerciais-kivy/aulas/aula10.py
@@ -0,0 +1,10 @@
+idade = int(input("Informe a sua idade: "))
+
+if idade <= 0:
+    print("Digite uma idade válida!")
+
+elif idade > 150:
+    print("Digite uma idade válida!")
+
+elif idade < 18:
+    print("Você precisa ter mais do que 18 anos!")
