1ca033fbfa6ac79187692175b7978505a400d056
Jean Pierre <jptr2189@gmail.com>
2023-12-27 20:23:49 -0200

    aula19 concluída

---
+++ excript/app-comerciais-kivy/aulas/aula19.py
@@ -0,0 +1,11 @@
+x = 10
+texto = "sim" if x == 10 else "não"
+
+print(texto)
+print('-' * 30)
+
+num1 = int(input("Digite um número: "))
+s = "PAR" if num1 % 2 == 0 else "ÍMPAR"
+
+print('-' * 30)
+print(f"O Número digitado é {s}")
