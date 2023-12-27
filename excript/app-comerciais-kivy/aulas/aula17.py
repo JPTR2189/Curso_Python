ec29abacb2e0949dd745b3dd6924437817ad033f
Jean Pierre <jptr2189@gmail.com>
2023-12-27 19:34:34 -0200

    aula16 concluída

---
+++ excript/app-comerciais-kivy/aulas/aula17.py
@@ -0,0 +1,26 @@
+# OPERADORES DE ATRIBUIÇÃO II
+
+y = 3
+x = 9
+x += y
+
+# x = 12
+print(x)
+
+x = 9
+x *= y
+
+# x = 27
+print(x)
+
+x = 9
+x /= y
+
+# x = 3
+print(x)
+
+x = 9
+x %= y
+
+# x = 0
+print(x)
