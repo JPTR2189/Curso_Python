0daa2d837fc7c4b33a9e5d7478f34903427e2a2a
Jean Pierre <jptr2189@gmail.com>
2024-01-01 18:44:58 -0200

    aula28 concluída

---
+++ excript/app-comerciais-kivy/aulas/aula28.py
@@ -0,0 +1,11 @@
+# LISTA
+
+lista = [['a', 'b', 'c'], [5, 7, 8], [True, False]]
+
+print(lista[0][2])
+print(lista[1][0])
+print(lista[2][1])
+print("-" * 40)
+
+print(f"O Tamanho dessa lista é '{len(lista)}'")
+print(f"O Último elemento dessa lista é '{lista[-1]}'")
