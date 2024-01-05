5122c5f6c16b3b74921ec7ccdd751b404e1274d7
Jean Pierre <jptr2189@gmail.com>
2024-01-05 16:13:51 -0200

    aula47 concluída

---
+++ excript/app-comerciais-kivy/aulas/aula47.py
@@ -0,0 +1,9 @@
+# FUNÇÕES - PARÂMETROS DEFAULT
+
+def login(sistema, usario="root", senha="123"):
+    print("Sistema:", sistema)
+    print("Usário:", usario)
+    print("Senha:", senha)
+
+
+login("Windows", "João", "41517")
