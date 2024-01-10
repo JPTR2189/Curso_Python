24070be30eee962f436878daf2c94286765e5c1e
Jean Pierre <jptr2189@gmail.com>
2024-01-10 21:53:58 -0200

    funções-ex003 concluído

---
+++ Exercicios/Funções/ex003.py
@@ -0,0 +1,8 @@
+# Escreva um algoritmo que receba dois números através da declaração de dois parâmetros cujos nomes serão:
+# x e y. No bloco de instrução faça a soma de ambos valores e imprima o resultado no monitor:
+
+def func(x, y):
+    print(f"A Soma de '{x}' e '{y}' é igual a '{x + y}'")
+
+
+func(2, 3)
