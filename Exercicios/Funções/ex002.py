fd0b5d728ee200a5c1e09fa2c62a3665f8ffdac3
Jean Pierre <jptr2189@gmail.com>
2024-01-10 21:51:37 -0200

    funções-ex002 concluído

---
+++ Exercicios/Funções/ex002.py
@@ -0,0 +1,10 @@
+# Escreva um código contendo uma função de nome estudo e defina que a mesma deva receber um número como argumento.
+# Chame este argumento de x. No corpo da função imprima a seguinte frase na tela: "Função invocada com sucesso.
+# O valor passado pelo argumento x é: <valor de x>"
+
+def estudo(x):
+    print("Função invocada com sucesso.")
+    print(f"O Valor passado pelo argumento 'x' é: {x}")
+
+
+estudo(3)
