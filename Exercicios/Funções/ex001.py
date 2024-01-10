cdd763292dcd446bea992299ab47235a76150020
Jean Pierre <jptr2189@gmail.com>
2024-01-10 21:49:12 -0200

    funções-ex001 concluído

---
+++ Exercicios/Funções/ex001.py
@@ -0,0 +1,8 @@
+# Escreva um algoritmo que contenha uma função de nome estudo() e que quando
+# executada imprima na saída padrão a frase "Estamos estudando as funções":
+
+def estudo():
+    print("Estamos estudando as funções!")
+
+
+estudo()
