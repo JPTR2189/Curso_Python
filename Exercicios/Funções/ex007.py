7fa2efc7bdbb5bb499ab12bd8195bb1b6736d7f9
Jean Pierre <jptr2189@gmail.com>
2024-01-11 16:02:09 -0200

    funções-ex007 concluído

---
+++ Exercicios/Funções/ex007.py
@@ -0,0 +1,8 @@
+# Escreva um algoritmo capaz de receber uma quantidade variável de parâmetros.
+# Em seguida, imprima os parâmetros recebidos na tela:
+
+def func(*params):
+    print(params)
+
+
+func(1, 2, 3, 4, 5, 6)
