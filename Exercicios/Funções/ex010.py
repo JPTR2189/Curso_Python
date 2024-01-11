4c4d83254348c5f376b39372fb85957e3b84aaa0
Jean Pierre <jptr2189@gmail.com>
2024-01-11 16:21:29 -0200

    funções-ex010 concluído

---
+++ Exercicios/Funções/ex010.py
@@ -0,0 +1,10 @@
+# Escreva um algoritmo que encontre o maior dentre 3 números.
+# Para facilitar a resolução do exercício utilize funções.
+
+def maior(lista):
+    print(f"O Maior número da lista é: '{max(lista)}'")
+
+
+lista =[2,9,6]
+
+maior(lista)
