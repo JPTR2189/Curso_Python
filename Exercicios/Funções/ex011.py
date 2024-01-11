53baa73e6bc4792ae01deb9eb6690be9f02f0685
Jean Pierre <jptr2189@gmail.com>
2024-01-11 16:24:17 -0200

    funções-ex011 concluído

---
+++ Exercicios/Funções/ex011.py
@@ -0,0 +1,11 @@
+# Escreva um função que some todos os números contidos numa lista.
+# Lista: (1, 2, 3, 4, 5)
+# Soma: 15
+
+def soma(lista):
+    print(f"A Soma dessa lista é igual a '{sum(lista)}'")
+
+
+lista = [1, 2, 3, 4, 5]
+
+soma(lista)
