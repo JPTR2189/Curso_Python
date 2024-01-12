a452b8ae67db7664b7f8cbd7c86b097cedc30e50
Jean Pierre <jptr2189@gmail.com>
2024-01-12 18:12:03 -0200

    Funções-ex015 concluído

---
+++ Exercicios/Funções/ex015.py
@@ -0,0 +1,14 @@
+# Escreva uma função que verifique se um número está num intervalo determinado.
+
+def intervalo(num):
+
+    if 0 <= num <= 10:
+
+        print(f"O número {num} está no intervalo de 1 até 10")
+
+    else:
+
+        print(f"O número '{num}' não está no intervalo de 1 até 10")
+
+
+intervalo(12)
