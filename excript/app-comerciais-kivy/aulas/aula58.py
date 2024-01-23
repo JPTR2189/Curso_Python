a095bdaa6750d14ea244e0a46765821cec68b182
Jean Pierre <jptr2189@gmail.com>
2024-01-23 18:37:27 -0200

    aula58 concluída

---
+++ excript/app-comerciais-kivy/aulas/aula58.py
@@ -0,0 +1,15 @@
+def func():
+    f1 = "teste"
+    print(__name__)
+
+
+a = 0
+print(varx)
+
+func()
+
+l = list()
+
+for x in [1,2]:
+    b = 0
+
--- main.py
+++ main.py
@@ -1,16 +1,1 @@
-import math
-
-def is_prime(n):
-    if n <= 1:
-        return False
-    for i in range(2, int(math.sqrt(n)) + 1):
-        if n % i == 0:
-            return False
-    return True
-
-# Exemplo de uso:
-numero = 100
-if is_prime(numero):
-    print(f"{numero} é primo.")
-else:
-    print(f"{numero} não é primo.")
+help(dir())
