58b5645512e1ea2806afc6d201b7cffc5cf872ed
Jean Pierre <jptr2189@gmail.com>
2024-01-24 19:54:30 -0200

    aula61 concluída

---
+++ excript/app-comerciais-kivy/aulas/aula61.py
@@ -0,0 +1,10 @@
+# CRIAÇÃO DE MÓDULOS
+
+# módulo main
+
+import aula61b
+
+# print("O Programa acabou!")
+
+print(__name__)
+
---
+++ excript/app-comerciais-kivy/aulas/aula61b.py
@@ -0,0 +1,16 @@
+# CRIAÇÃO DE MÓDULOS
+
+# módulo estudo
+
+def f():
+    print("f1")
+    print("f2")
+    print("f3")
+    print("f4")
+
+a = 10
+b = "Módulo personalizado"
+
+# print("Módulo executado")
+
+print(__name__)
