c62a878618455536b9257ddc7f7d9dd534b0cf54
Jean Pierre <jptr2189@gmail.com>
2024-01-27 17:14:12 -0600

    Exercicios_extras-ex021 concluído

---
+++ Exercicios_Extras/ex021.py
@@ -0,0 +1,13 @@
+# Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3
+import pygame
+
+pygame.init()
+
+pygame.mixer.music.load("aula21b.mp3")
+
+pygame.mixer.music.play()
+
+while pygame.mixer.music.get_busy():
+    pygame.time.Clock().tick(10)  
+
+pygame.quit()
