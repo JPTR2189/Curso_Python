8869cab8996126297e319d41813c103482426706
Jean Pierre <jptr2189@gmail.com>
2024-01-27 15:55:00 -0600

    Exercicios_extras-ex016 concluído

---
+++ Exercicios_Extras/ex016.py
@@ -0,0 +1,4 @@
+#  Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.
+
+num = float(input("Digite um número REAL: "))
+print(f"O número digitado tem a parte '{int(num)}' inteira")
