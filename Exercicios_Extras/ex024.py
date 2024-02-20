1b3ff9be54960b40fac3f1f49fc987a495a4929c
Jean Pierre <jptr2189@gmail.com>
2024-02-19 19:34:52 -0600

    Exercicios_Extras-ex024 concluído

---
+++ Exercicios_Extras/ex024.py
@@ -0,0 +1,12 @@
+# Crie um programa que leia o nome de uma cidade diga se ela começa ou não com o nome "SANTO".
+
+palavra = input("Digite a cidade que você nasceu: ").upper().strip()
+
+if palavra[0:5] == "SANTO":
+    print('-' * 30)
+    print("A cidade que você nasceu começa com o nome 'Santo'")
+
+else:
+    print('-' * 30)
+    print("A cidade que você nasceu NÃO começa com o nome santo")
+    
